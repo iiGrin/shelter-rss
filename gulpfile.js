ff8ce1a0a9d3aaf1ac6bfb4ff65e4429ce84db4d
devIggy <devdevgrin@gmail.com>
2024-05-21 16:58:22 +0300

    feat: header 1280px & more

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ gulp.task("server", function () {
 
 gulp.task("styles", function () {
 	return gulp
-		.src("src/sass/**/*.+(scss|sass)")
+		.src("src/sass/**/*.+(scss|sass|css)")
 		.pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
 		.pipe(rename({ suffix: ".min", prefix: "" }))
 		.pipe(autoprefixer())
