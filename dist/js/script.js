4ad1e8cc4053d4294a8e8fa2514dd9496872f99c
devIggy <devdevgrin@gmail.com>
2024-07-05 10:52:26 +0300

    feat: popup action

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,17 +1,34 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const navMenu = document.querySelector(".nav-menu"),
-      navMenuLink = document.querySelectorAll(".nav-menu__link"),
-      hamburger = document.querySelector(".hamburger");
-  
-    hamburger.addEventListener("click", () => {
-      hamburger.classList.toggle("hamburger_active");
-      navMenu.classList.toggle("nav-menu_active");
-    });
-  
-    navMenuLink.forEach((item) => {
-      item.addEventListener("click", () => {
-        hamburger.classList.remove("hamburger_active");
-        menu.classList.remove("nav-menu_active");
-      });
+  const navMenu = document.querySelector(".nav-menu"),
+    navMenuLink = document.querySelectorAll(".nav-menu__link"),
+    hamburger = document.querySelector(".hamburger"),
+    body = document.querySelector("body"),
+    popup = document.querySelector(".popup"),
+    popupButton = document.querySelectorAll(".button_popup"),
+    popupClose = document.querySelector(".close-popup");
+    ;
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger_active");
+    navMenu.classList.toggle("nav-menu_active");
+  });
+
+  navMenuLink.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.remove("hamburger_active");
+      menu.classList.remove("nav-menu_active");
     });
   });
+
+  popupButton.forEach((item) => {
+    item.addEventListener("click", () => {
+      popup.classList.toggle("popup_active");
+      body.classList.toggle("body_lock");
+    })
+  });
+
+  popupClose.addEventListener("click", () => {
+    popup.classList.remove("popup_active");
+    body.classList.remove("body_lock");
+  });
+});
